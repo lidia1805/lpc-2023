5f83f06fef19d89123e69dda34dd4e3efc45983c
Larry Amaral Reis <98787617+LarryAmaralReis@users.noreply.github.com>
2022-02-12 18:41:58 -0300

    Update projeto_pong.py

--- projeto_pong.py
+++ projeto_pong.py
@@ -4,16 +4,12 @@ pygame.mixer.init()
 pygame.mixer.music.load('LPC-2022/Piano.mp3')
 pygame.mixer.music.play(-1)
 
-#teste
-
-# draw screen
 screen = turtle.Screen()
 screen.title("My Pong")
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.tracer(0)
 
-# draw paddle_1
 paddle_1 = turtle.Turtle()
 paddle_1.speed(0)
 paddle_1.shape("square")
@@ -22,7 +18,6 @@ paddle_1.shapesize(stretch_wid=5, stretch_len=1)
 paddle_1.penup()
 paddle_1.goto(-350, 0)
 
-# draw paddle_2
 paddle_2 = turtle.Turtle()
 paddle_2.speed(0)
 paddle_2.shape("square")
@@ -31,7 +26,6 @@ paddle_2.shapesize(stretch_wid=5, stretch_len=1)
 paddle_2.penup()
 paddle_2.goto(350, 0)
 
-# draw ball
 ball = turtle.Turtle()
 ball.speed(0)
 ball.shape("square")
@@ -41,7 +35,6 @@ ball.goto(0, 0)
 ball.dx = 1
 ball.dy = 1
 
-#head-up display
 hud = turtle.Turtle()
 hud.speed(0)
 hud.shape("square")
@@ -49,7 +42,8 @@ hud.color("white")
 hud.penup()
 hud.hideturtle()
 hud.goto(0, 260)
-hud.write("0 : 0", align="center", font=("Press Start 2P",24, "normal"))
+hud.write("0 : 0", align="center", font=("Press Start 2P", 24, "normal"))
+
 
 def paddle_1_up():
     y = paddle_1.ycor()
@@ -59,13 +53,15 @@ def paddle_1_up():
         y = 250
     paddle_1.sety(y)
 
+
 def paddle_1_down():
     y = paddle_1.ycor()
     if y > -250:
         y += -30
     else:
-        y = -250 #y <=30
+        y = -250
     paddle_1.sety(y)
+
 
 def paddle_2_up():
     y = paddle_2.ycor()
@@ -75,6 +71,7 @@ def paddle_2_up():
         y = 250
     paddle_2.sety(y)
 
+
 def paddle_2_down():
     y = paddle_2.ycor()
     if y > -250:
@@ -83,60 +80,50 @@ def paddle_2_down():
         y = -250
     paddle_2.sety(y)
 
-# keyboard
+
 screen.listen()
 screen.onkeypress(paddle_1_up, "w")
 screen.onkeypress(paddle_1_down, "s")
 screen.onkeypress(paddle_2_up, "Up")
 screen.onkeypress(paddle_2_down, "Down")
 
-# score
 score_1 = 0
 score_2 = 0
 
 while True:
     screen.update()
 
-    # ball movement
-    ball.sety(ball.xcor() + ball.dx)
-    ball.sety(ball.ycor() + ball.dx)
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
 
-    #collision with the upper wall
     if ball.ycor() > 290:
-         # os.system("afplay bounce.wav&")
-         ball.sety(290)
-         ball.dy *= -1
+        ball.sety(290)
+        ball.dy *= -1
 
-    #collision with the lower wall
     if ball.ycor() < -290:
-         # os.system("afplay bounce.wav&")
-         ball.sety(-290)
-         ball.dy *= -1
+        ball.sety(-290)
+        ball.dy *= -1
 
-    #collision with the paddle 1
-    if ball.xcor() < -330 and paddle_1.ycor() + 50 > ball.ycor() > paddle_1.ycor() - 50:
+    if ball.xcor() < -330 and paddle_1.ycor() + 50 >\
+            ball.ycor() > paddle_1.ycor() - 50:
         ball.dx *= -1
-        #os.system("afplay bounce.wav&")
 
-    # collision with the paddle 2
-    if ball.xcor() > 330 and paddle_2.ycor() + 50 > ball.ycor() > paddle_2.ycor() - 50:
+    if ball.xcor() > 330 and paddle_2.ycor() + 50 >\
+            ball.ycor() > paddle_2.ycor() - 50:
         ball.dx *= -1
-        # os.system("afplay bounce.wav&")
 
-    #collision with left wall
     if ball.xcor() < -390:
         score_2 += 1
         hud.clear()
-        hud.write(f"{score_1} : {score_2}", align="center", font=("Small Fonts", 24, "normal"))
-        #os.system("ofplay 258020__kodack__arcade-bleep-sound.wav&)
+        hud.write("{} : {}".format(score_1, score_2),
+                  align="center", font=("Press Start 2P", 24, "normal"))
         ball.goto(0, 0)
         ball.dx *= -1
 
-    #collision with right wall
     if ball.xcor() > 390:
         score_1 += 1
         hud.clear()
-        hud.write(f"{score_1} : {score_2}", align="center", font=("Small Fonts", 24, "normal"))
-        #os.system("ofplay 258020__kodack__arcade-bleep-sound.wav&)
+        hud.write("{} : {}".format(score_1, score_2),
+                  align="center", font=("Press Start 2P", 24, "normal"))
         ball.goto(0, 0)
         ball.dx *= -1
