7533bb630082de27ed547c95450d651d1b7d815c
Luana <lmd.snf21@uea.edu.br>
2022-02-14 00:02:30 -0400

    player red

--- projeto_pong.py
+++ projeto_pong.py
@@ -19,7 +19,7 @@ screen.tracer(0)
 paddle_1 = turtle.Turtle()
 paddle_1.speed(0)
 paddle_1.shape("square")
-paddle_1.color("white")
+paddle_1.color("green")
 paddle_1.shapesize(stretch_wid=5, stretch_len=1)
 paddle_1.penup()
 paddle_1.goto(-350, 0)
@@ -27,7 +27,7 @@ paddle_1.goto(-350, 0)
 paddle_2 = turtle.Turtle()
 paddle_2.speed(0)
 paddle_2.shape("square")
-paddle_2.color("white")
+paddle_2.color("green")
 paddle_2.shapesize(stretch_wid=5, stretch_len=1)
 paddle_2.penup()
 paddle_2.goto(350, 0)
@@ -44,11 +44,12 @@ ball.dy = 1
 hud = turtle.Turtle()
 hud.speed(0)
 hud.shape("square")
-hud.color("white")
+hud.color("red")
 hud.penup()
 hud.hideturtle()
 hud.goto(0, 260)
-hud.write("0 : 0", align="center", font=("Press Start 2P", 24, "normal"))
+hud.write("Player 0 : Player 0", align="center", font=("Press Start 2P", 24, "normal"))
+
 
 
 def paddle_1_up():
@@ -103,14 +104,14 @@ while True:
     ball.sety(ball.ycor() + ball.dy)
 
     if ball.ycor() > 290:
+        play()
         ball.sety(290)
         ball.dy *= -1
-        play()
 
     if ball.ycor() < -290:
+        play()
         ball.sety(-290)
         ball.dy *= -1
-        play()
 
     if ball.xcor() < -330 and paddle_1.ycor() + 50 > \
             ball.ycor() > paddle_1.ycor() - 50:
@@ -125,17 +126,17 @@ while True:
     if ball.xcor() < -390:
         score_2 += 1
         hud.clear()
-        hud.write("{} : {}".format(score_1, score_2),
+        hud.write("Player 1: {}     Player 2: {}".format(score_1, score_2),
                   align="center", font=("Press Start 2P", 24, "normal"))
+        play()
         ball.goto(0, 0)
         ball.dx *= -1
-        play()
 
     if ball.xcor() > 390:
         score_1 += 1
         hud.clear()
-        hud.write("{} : {}".format(score_1, score_2),
+        hud.write("Player 1: {}     Player 2: {}".format(score_1, score_2),
                   align="center", font=("Press Start 2P", 24, "normal"))
+        play()
         ball.goto(0, 0)
         ball.dx *= -1
-        play()
